72c52a0fb29dd354cc120cbb1ee4805e49dcc083
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-11-15 11:35:16 +0100

    Reforged checkCookie

--- JavaScript/cookie.js
+++ JavaScript/cookie.js
@@ -26,10 +26,7 @@ function setCookie(cname, cvalue, exdays) {
     if (username != "") {
      alert("Welcome again " + username);
     } else {
-      username = prompt("Please enter your name:", "");
-      if (username != "" && username != null) {
-        setCookie("username", username, 365);
-      }
+      alert("No username cookie");
     }
   }
   function deleteCookie(){
